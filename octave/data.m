fb878437ec5b8d6b4628d9c025cf0d6e5d66f3ae
krasnikovra <roma_krasnikov02@mail.ru>
2023-05-21 20:10:24 +0300

    initial

---
+++ octave/data.m
@@ -0,0 +1,15 @@
+pkg load interval
+
+input = csvread("Channel_1_500nm_0_23mm.csv")
+eps = 1e-4
+
+input_int = infsup(input - eps, input + eps)
+
+errorbar(mid(input_int), rad(input_int), "b")
+title("Data")
+xlabel("n")
+ylabel("mV")
+xlim([0, size(input, 1)])
+
+print -dpdfcrop ../figures/data_int.pdf
+
